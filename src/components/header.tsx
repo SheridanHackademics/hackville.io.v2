1be776ff99d9c5fd65ec4c85c48fbf8b32eedfd3
Andres Munevar <afmunevarm@hotmail.com>
2020-09-28 22:03:48 -0400

    Add global.css and header.module.css files

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,53 +1,23 @@
 import { Link } from "gatsby"
 import React from "react"
-import styled from "styled-components"
-// Styles are temporary
-const NavBar = styled.nav
-  `
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 96px;
-    background: var(--unnamed-color-000000) 0% 0% no-repeat padding-box;
-    background: #000000 0% 0% no-repeat padding-box;
-    opacity: 1;
-  `
-
-const Ul = styled.ul
-  `
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    width: 525px;
-    opacity: 1;
-    float: right;
-    display: flex;
-  `
-
-const Li = styled.li
-  `
-    margin: 2px;
-  `
-const linkStyle = {
-  color: '#white'
-}
+import headerStyles from "./header.module.css"
 
 const Header = () => (
   <header>  
-      <NavBar>
+      <nav className={headerStyles.navbar}>
         <div>
           <div >
-          <Link to='#'>Hackademic Logo</Link>
+          <Link className="navbar-logo" to='#'>Hackademics Logo</Link>
         </div>
-        <Ul>
-          <Li><Link style={{ color: `#fff` }} to='#about'>About</Link></Li>
-          <Li><Link style={{ color: `#fff` }} to='#previous-year'>2019</Link></Li>
-          <Li><Link style={{ color: `#fff` }} to='#faq'>FAQ</Link></Li>
-          <Li><Link style={{ color: `#fff` }} to='#get-involved'>Get Involved</Link></Li>
-          <Li><Link style={{ color: `#fff` }} to='#sponsors'>Sponsors</Link></Li>
-        </Ul>
+        <ul className={headerStyles.navbarUl}>
+          <li><Link style={{ color: `#fff` }} to='#about'>About</Link></li>
+          <li><Link style={{ color: `#fff` }} to='#previous-year'>2019</Link></li>
+          <li><Link style={{ color: `#fff` }} to='#faq'>FAQ</Link></li>
+          <li><Link style={{ color: `#fff` }} to='#get-involved'>Get Involved</Link></li>
+          <li><Link style={{ color: `#fff` }} to='#sponsors'>Sponsors</Link></li>
+        </ul>
       </div>
-      </NavBar>
+      </nav>
     
   </header>
 )
