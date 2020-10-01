ddff97e271150bd32d43e02bff1b249110cff0a4
Andres Munevar <afmunevarm@hotmail.com>
2020-10-01 00:09:29 -0400

    Add footer images and logos

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,20 +1,22 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import footerStyles from "./footer.module.css"
+import logoInitial from "../../images/logos/ha-logo-bw-white-SVG.svg"
+import facebook from "../../images/footer/facebook.svg"
+import instagram from "../../images/footer/instagram.svg"
 
-const Footer = ({organization}) => (
-  <footer>
-      <Link to='/contact-us/'>Contact Us</Link>
-      {organization}
+const Footer = () => (
+  <footer className={footerStyles}>
+      <ul>
+        <li><Link to="/contact-us/">Contact Us</Link></li>
+        <li><Link to="">Sponsorship Package</Link></li>
+      </ul>
+      <img src={logoInitial}></img>
+      <div className={footerStyles.socials}>
+        <Link to=""><img src={facebook}></img></Link>
+        <Link to=""><img src={instagram}></img></Link>        
+      </div>
   </footer>
 )
-
-Footer.propTypes = {
-  organization: PropTypes.string,
-}
-
-Footer.defaultProps = {
-  organization: `Hackademics`
-}
 
 export default Footer
