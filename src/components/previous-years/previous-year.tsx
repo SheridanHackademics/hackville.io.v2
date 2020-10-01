ff23eeb4f4b056d61599bdac8a0903fafd6e3d4c
Andres Munevar <afmunevarm@hotmail.com>
2020-09-30 23:19:56 -0400

    Add icons for 2019 recap

--- src/components/previous-years/previous-year.tsx
+++ src/components/previous-years/previous-year.tsx
@@ -1,8 +1,40 @@
 import React from "react"
+import previousStyles from "./previous-year.module.css"
+import recap from "../../images/headers/recap-header.svg"
+import recapHackers from "../../images/hackers-star.svg"
+import recapHours from "../../images/hours-triangle.svg"
+import recapProjects from "../../images/projects-triangle.svg"
+import recapPrizes from "../../images/prizes-blob.svg"
 
+//Probably rename to recap and code for recapData can probably be refactored
 const PreviousYear = () => (
-    <section id="previous-year" style={{margin: `768px 0px`}}>
-      <h1> 2019 </h1>
+    <section id="previous-year" className={previousStyles.recapSection}>
+      <div className={previousStyles.recapHeader}>
+        <img src={recap}></img>
+        <p>hackville 2019 recap</p>
+      </div>
+      <div className={previousStyles.recapData}>
+        <div className={previousStyles.data}>
+          <img src={recapHackers}></img>
+          <p>100</p>
+          <p>Hackers</p>
+        </div>
+        <div className={previousStyles.data}>
+          <img src={recapHours}></img>
+          <p>42</p>
+          <p>Hours</p>
+        </div>
+        <div className={previousStyles.data}>
+          <img src={recapProjects}></img>
+          <p>23</p>
+          <p>Projects</p>
+        </div>
+        <div className={previousStyles.data}>
+          <img src={recapPrizes}></img>
+          <p>$4k</p>
+          <p>Prizes</p>
+        </div>
+      </div>
     </section>
 )
 
